ddf69f44ba0fc58128e0a5687b5a56ba5afab855
aman <15uec012@lnmiit.ac.in>
2020-08-06 16:06:57 +0530

    live video feature added using obs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route} from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
@@ -7,20 +7,20 @@ import StreamList from "./streams/StreamList";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
-import history from '../history'
+import history from "../history";
 
 function App() {
 	return (
 		<div className="ui container">
 			<Router history={history}>
 				<Header />
-				<div>
+				<Switch>
 					<Route path="/" exact component={StreamList} />
 					<Route path="/streams/new" exact component={StreamCreate} />
 					<Route path="/streams/edit/:id" exact component={StreamEdit} />
 					<Route path="/streams/delete/:id" exact component={StreamDelete} />
-					<Route path="/streams/show" exact component={StreamShow} />
-				</div>
+					<Route path="/streams/:id" exact component={StreamShow} />
+				</Switch>
 			</Router>
 		</div>
 	);
